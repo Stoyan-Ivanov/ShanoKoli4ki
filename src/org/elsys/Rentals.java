61a760449e1caf7ff6db83c66559777375e6d527
Stoyan-Ivanov <stoyaniviv@gmail.com>
2017-06-21 00:35:40 +0300

    add add rental

--- src/org/elsys/Rentals.java
+++ src/org/elsys/Rentals.java
@@ -24,8 +24,91 @@ public class Rentals {
 	
 	
 	public void addRental () {
+		String regPlate;
 		
+		try {
+			System.out.println("Welcome to our rental platform");
+			System.out.println("---------------------------\n");
+			System.out.println("Please enter your email address: ");
+			
+			String email = "'" + scanner.nextLine() + "'";
+			
+			stmt = conn.createStatement();
+			
+			String sql = "SELECT * FROM shanokoli4ki.people WHERE Email = " + email;
+			
+			ResultSet result = stmt.executeQuery(sql);
+			System.out.println(result);
+			
+			if(!result.next()) {
+				System.out.println("\n Please make sure that you have made an account! \n");
+				
+				System.out.println("Press Enter to continue...");
+		        scanner.nextLine();
+		        
+			} else {
+				
+				System.out.println("\n Login successfull!\n");
+				System.out.print("Enter a registration plate of the car: ");
+				
+				regPlate = "'" + scanner.nextLine() + "'";
+				
+				stmt = conn.createStatement();
+				
+				sql = "SELECT * FROM shanokoli4ki.car WHERE RegistrationPlate = " + regPlate;
+				
+				result = stmt.executeQuery(sql);
+				
+				if(!result.next()) {
+					System.out.println("\n Please make sure that the registartion plate is valid!\n");
+					
+					System.out.println("Press Enter to continue...");
+			        scanner.nextLine();
+			        
+				} else {
+					
+					System.out.print("Rent a car from : ");
+					String fromDate = "'" + scanner.nextLine() + "'";
+					
+					System.out.print("To:");
+					String toDate = "'" + scanner.nextLine() + "'";
+					
+					stmt = conn.createStatement();
+					
+					sql = "SELECT * FROM shanokoli4ki.rents WHERE 'RegistrationPlate' = " + regPlate + " AND " +
+					"'FromDate' >= " + fromDate + " AND " + "'ToDate' <= " + toDate ;
+					
+					
+					result = stmt.executeQuery(sql);
+					System.out.println("tuka vliza");
+					
+					if(!result.next()) {
+						System.out.println("tuka syshto vliza");
+						stmt = conn.createStatement();
+						
+						sql = "INSERT INTO shanokoli4ki.rents (RegistrationPlate, Email, FromDate, ToDate) VALUES(" + regPlate + " ," 
+								+ email + " ," + fromDate + " ," + toDate + ")";
+						
+						stmt.executeUpdate(sql);
+						
+						System.out.println("\n* Rental successfully placed! * \n");
+						
+						System.out.println("Press Enter to continue...");
+				        scanner.nextLine();
+				        
+					} else {
+						
+						System.out.println("\n The car is not available for the date range you have provided! \n");
+						
+						System.out.println("Press Enter to continue...");
+				        scanner.nextLine();
+					}
+				}	
+			}
+		} catch (SQLException e) {}
+			
 	}
+			
 	
 	public void removeRental() {
 		
